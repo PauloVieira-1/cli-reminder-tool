05331b65426bce49a69b0fd2d28180a6e15a4a0e
Paulo Vieira <paulo1vieira.2004@gmail.com>
2025-08-03 20:17:47 +0100

    .

--- src/data_manager.rs
+++ src/data_manager.rs
@@ -1,10 +1,11 @@
 use dirs;
 use std::path::PathBuf;
 use crate::reminder::Reminder;
+use std::result;
 
 fn get_data_file_path() -> PathBuf {
     let data_dir = dirs::config_dir().expect("Could not determine config directory");
-    println!("Data directory: {}", data_dir.display());
+    // println!("Data directory: {}", data_dir.display());
     let reminders_dir = data_dir.join("cli_reminder_tool");
     if !reminders_dir.exists() {
         std::fs::create_dir_all(&reminders_dir).expect("Could not create reminders directory");
@@ -28,3 +29,38 @@ pub fn save_reminder_to_file(reminder: &Reminder) {
 
     println!("Reminder saved successfully!");
 }
+
+pub fn get_remdiners() -> Vec<Reminder> {
+    let file_path = get_data_file_path();
+    if !file_path.exists() {
+        return vec![];
+    }
+
+    let data = std::fs::read_to_string(&file_path).expect("Could not read data file");
+    serde_json::from_str(&data).expect("Could not parse data file")
+
+}
+
+pub fn remove_reminder(id: i32) -> Result<(), Box<dyn std::error::Error>> {
+    let file_path = get_data_file_path();
+    let mut reminders: Vec<Reminder> = vec![];
+
+    if file_path.exists() {
+        let data = std::fs::read_to_string(&file_path).expect("Could not read data file");
+        reminders = serde_json::from_str(&data).expect("Could not parse data file");
+    }
+
+    if reminders.iter().all(|r| r.id != id) { // returns true for all elements in closure if id != id. If even one false is returned, returns false
+        println!("No reminders found with the specified ID.");
+        return Ok(());
+    }
+
+    reminders.retain(|r| r.id != id);
+
+    let serialized_data = serde_json::to_string(&reminders).expect("Could not serialize reminders");
+    std::fs::write(file_path, serialized_data).expect("Could not write to data file");
+    println!("Reminder with ID {} removed successfully!", id);
+
+    Ok(())
+}
+
--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,23 @@ mod data_manager;
 
 use std::env::args;
 use reminder::{Reminder, CommandType};
-use data_manager::save_reminder_to_file;
+use data_manager::{save_reminder_to_file, get_remdiners, remove_reminder};
 use rand::Rng;
 
-fn main() {
-    let args: Vec<String> = args().collect();
 
-    if !check_args(&args) {
+fn main() {
+    let arguments: Vec<String> = args().collect();
+
+    if !check_args(&arguments) {
         return;
     }
 
-    let command_str = get_command(&args[1]);
-    let id = rand::thread_rng().gen_range(1..100000);
-    let reminder = Reminder::new(id, args[2].clone(), args[3].clone(), args[4].clone());
-   
+    let command_str = get_command(&arguments[1]);
+
     if let Some(command) = command_str {
-        handle_command(command, reminder);
+        handle_command(command, &arguments);
     } else {
-        eprintln!("Invalid command: {}", args[1]);
+        eprintln!("Invalid command: {}", arguments[1]);
     }
 }
 
@@ -34,22 +33,69 @@ fn get_command(command_str: &str) -> Option<CommandType> {
     }
 }
 
-fn handle_command(command: CommandType, reminder: Reminder) {
+fn handle_command(command: CommandType, args: &[String]) {
     match command {
         CommandType::Add => {
             println!("Adding a new reminder...");
+            let id = rand::thread_rng().gen_range(1..100000);
+            let reminder = Reminder::new(id, args[2].clone(), args[3].clone(), args[4].clone());
             save_reminder_to_file(&reminder);
         }
-        CommandType::List => println!("Listing all reminders..."),
-        CommandType::Remove => println!("Removing a reminder..."),
+        CommandType::List => {
+            println!("\n#################################");
+            println!("Listing all reminders:");
+            println!("#################################\n");
+            
+            let reminders = data_manager::get_remdiners();
+            
+            if reminders.is_empty() {
+                println!("No reminders found.");
+            }
+            
+            println!("----------------------------------------------------------------------");
+            for r in reminders {
+                println!("ID: {}, Title: {}, Description: {}, Due Date: {}", r.id, r.title, r.description, r.due_date);
+            println!("----------------------------------------------------------------------");
+            }
+            
+            println!("\n")
+        }
+        CommandType::Remove => {
+            println!("Removing a reminder...");
+            data_manager::remove_reminder(args[2].parse().unwrap()).expect("Failed to remove reminder");
+        }
         CommandType::Update => println!("Updating a reminder..."),
     }
 }
 
 fn check_args(args: &[String]) -> bool {
-    if args.len() < 5 {
-        eprintln!("Usage: <command> <title> <description> <due_date>");
-        return false;
+    let command = &args[1].to_lowercase();
+
+    match args.len() {
+        2 => {
+            if command == "list" {
+                true
+            } else {
+                println!("Usage: cli_reminder_tool list");
+                false
+            }
+        }
+        3 => {
+            if command == "remove" || command == "update" {
+                true
+            } else {
+                println!("Usage: cli_reminder_tool remove <id> | update <id> <title> <description> <due_date>");
+                false
+            }
+        }
+        _ => {
+            if args.len() < 5 {
+                println!("Usage: cli_reminder_tool add <title> <description> <due_date>");
+                return false;
+            }
+
+            true
+        }
     }
-    true
 }
+
